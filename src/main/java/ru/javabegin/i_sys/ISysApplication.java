9df58e8e885da6fac626fc5e16627623cd01d388
“stacenko63” <amr_stacenko@mail.ru>
2022-06-02 23:42:10 +0400

    fix: fix errors in swagger

--- src/main/java/ru/javabegin/i_sys/ISysApplication.java
+++ src/main/java/ru/javabegin/i_sys/ISysApplication.java
@@ -3,6 +3,8 @@ package ru.javabegin.i_sys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -11,6 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableWebMvc
+@Configuration
 public class ISysApplication {
 
 	public static void main(String[] args) {
@@ -20,8 +24,7 @@ public class ISysApplication {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("ru.javabegin.i_sys")).build();
+				.apis(RequestHandlerSelectors.basePackage("ru.javabegin")).build();
 	}
-
 
 }
